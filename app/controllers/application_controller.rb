1fe4b97a8edd144a6c81931b0dbdfa7c795ae0e6
Ralph <ralphoburu@gmail.com>
2021-04-12 15:05:58 +0300

    Add user signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Welcome #{@user.name}! You can now login."
+    else
+      flash.now.alert = "Sorry, we couldn't process your request."
+      render :new
+    end
+  end
 end
