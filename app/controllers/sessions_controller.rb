a6baa18086ed7a988f5135b268fea54227406ac8
Ralph <ralphoburu@gmail.com>
2021-04-12 17:21:23 +0300

    Add most voted scope

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
   def new
+    redirect_to categories_path if current_user
   end
 
   def create
     @user = User.find_by(name: user_params[:name])
     if @user
       session[:user_id] = @user.id
-      redirect_to categories_path, notice: "Welcome #{@user.name}! "
+      redirect_to categories_path, notice: "Welcome #{@user.name}!"
     else
       flash.now.alert = 'Sorry, the user name is incorrect.'
       render :new
