d8c34748b1d782dc33ee9fed5503c0a063ada3cf
Ralph <ralphoburu@gmail.com>
2021-04-12 17:06:51 +0300

    Add voting logic

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
 
+  scope :order_by_most_recent, -> { order(updated_at: :desc) }
+
   belongs_to :author, class_name: 'User'
   has_many :votes
   has_and_belongs_to_many :categories
