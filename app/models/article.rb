4c22de64a8a7dacb8725a1edabac205fd1a5ec04
Ralph <ralphoburu@gmail.com>
2021-04-12 15:30:39 +0300

    Add model validations and assosiations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
 
+  belongs_to :author, class_name: 'User'
+  has_many :votes
+  has_and_belongs_to_many :categories
 end
