da13099795d2644b2b58a7cee4793a4d85db5154
Ralph <ralphoburu@gmail.com>
2021-04-12 15:14:52 +0300

    Add sessions control for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: user_params[:name])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to categories_path, notice: "Welcome #{@user.name}! "
+    else
+      flash.now.alert = 'Sorry, the user name is incorrect.'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: "Goodbye! You're now logged out!"
+  end
 end
