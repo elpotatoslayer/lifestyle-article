e333ac321806e65e67a7e950d3c5141e189f15b4
Ralph <ralphoburu@gmail.com>
2021-04-12 17:14:19 +0300

    Add article views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,14 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @category = Category.find(params[:cat])
+    @articles = @category.articles.order_by_most_recent.includes(:author)
+    @category.priority += 1
+    @category.save
+  end
+
+  def new
+    @article = current_user.articles.build
+    @categories = Category.all
+  end
 end
